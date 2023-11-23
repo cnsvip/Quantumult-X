9c1880b4fb3b24b8429b9f874ffb486b9c4e1ff8
cnsvip <122255879+cnsvip@users.noreply.github.com>
2023-11-23 17:33:25 +0800

    Update taozhivip.js

--- taozhivip.js
+++ taozhivip.js
@@ -17,9 +17,20 @@ hostname = *.taozhi.*
 
 var ojbk = JSON.parse($response.body);
 const vipa = '/order/client/order_form/isVip';
+const buya = 'order/client/order_form/is/buy';
+const admin = '/userRole/isAdminManager';
+
 
 
 if ($request.url.indexOf(vipa) != -1){
+  ojbk.data = true;
+}
+
+if ($request.url.indexOf(buya) != -1){
+  ojbk.data.success = true;
+}
+
+if ($request.url.indexOf(admin) != -1){
   ojbk.data = true;
 }
 
