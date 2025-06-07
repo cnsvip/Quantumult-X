ec589ac11114875f39e7edaca181370890525153
cnsvip <122255879+cnsvip@users.noreply.github.com>
2025-06-07 20:10:14 +0800

    Update shootz.js

--- scripts/shootz.js
+++ scripts/shootz.js
@@ -1,10 +1,29 @@
+/******************************
+
+测试
+
+
+*******************************
+
+[rewrite_local]
+
+^https?:\/\/.*\.(shootz\.*|shootz\.tech) url script-response-body https://github.com/cnsvip/Quantumult-X/blob/main/scripts/shootz.js
+
+[mitm] 
+
+hostname = *.shootz.*
+
+*************************************/
+
 var ojbk = JSON.parse($response.body);
 const vipa = '/user-api/mini/user/info';
 
 
+
 if ($request.url.indexOf(vipa) != -1){
-  ojbk.topVip = true;
+    ojbk.topVip = true;
   ojbk.arenaVip = true;
 }
+
 
 $done({body : JSON.stringify(ojbk)});
