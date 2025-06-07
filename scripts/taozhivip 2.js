6f24ffbb1d782aa94cff2ef7886ca091663112bc
cnsvip <122255879+cnsvip@users.noreply.github.com>
2025-06-07 20:20:40 +0800

    Add files via upload

---
+++ scripts/taozhivip 2.js
@@ -0,0 +1,43 @@
+/******************************
+
+淘知测试
+
+
+*******************************
+
+[rewrite_local]
+
+^https?:\/\/.*\.(taozhi\.*|taozhi\.cn) url script-response-body https://raw.githubusercontent.com/cnsvip/Quantumult-X/dev/taozhivip.js
+
+[mitm] 
+
+hostname = *.taozhi.*
+
+*************************************/
+
+var ojbk = JSON.parse($response.body);
+const vipa = '/order/client/order_form/isVip';
+const buya = 'order/client/order_form/is/buy';
+const admin = '/userRole/isAdminManager';
+
+const play = '/commodity/isPlay';
+
+
+
+if ($request.url.indexOf(vipa) != -1){
+  ojbk.data = true;
+}
+
+if ($request.url.indexOf(buya) != -1){
+  ojbk.data.success = true;
+}
+
+if ($request.url.indexOf(admin) != -1){
+  ojbk.data = true;
+}
+
+if ($request.url.indexOf(play) != -1){
+  ojbk.data.isPlay = true;
+}
+
+$done({body : JSON.stringify(ojbk)});
