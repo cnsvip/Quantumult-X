5f4cecdf931b3bef622afac9ef6e9a2f8bafef52
cnsvip <122255879+cnsvip@users.noreply.github.com>
2025-07-02 17:01:42 +0800

    Update shootz.js

--- scripts/shootz.js
+++ scripts/shootz.js
@@ -17,17 +17,22 @@ hostname = *.shootz.*
 
 var ojbk = JSON.parse($response.body);
 const vipa = '/user-api/mini/user/info';
+const vipb = '/payment-api/mini/vip/video';
 
 
 
 if ($request.url.indexOf(vipa) != -1){
     ojbk.topVip = true;
-  ojbk.arenaVip = true;
+    ojbk.arenaVip = true;
     ojbk.arenaVipExpireTime = "2030-01-01 00:00:00";
-ojbk.expireTime = "2030-01-01 00:00:00";
-ojbk.vipExpireDate = "2030-01-01 00:00:00";
-ojbk.vipExpiredDays = 0;
+    ojbk.expireTime = "2030-01-01 00:00:00";
+    ojbk.vipExpireDate = "2030-01-01 00:00:00";
+    ojbk.vipExpiredDays = 0;
 }
 
+if ($request.url.indexOf(vipb) != -1){
+    ojbk.svip = 1;
+    ojbk.vip = 1;
+}
 
 $done({body : JSON.stringify(ojbk)});
